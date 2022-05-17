9aeb165ad123a7623f23549ef789cf4107ef4412
lucaiyu <53391072+lucaiyu@users.noreply.github.com>
2022-05-17 16:48:28 +0800

    Add files via upload

---
+++ src/main/java/com/lucaiyu/touhoucraft/blocks/GensokyoPortalFrame.java
@@ -0,0 +1,11 @@
+package com.lucaiyu.touhoucraft.blocks;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+//public class GensokyoPortalFrame extends Block {
+    //public GensokyoPortalFrame() {
+        //super(Properties().create(Material.PORTAL).hardnessAndResistance(5));
+    //}
+//}
