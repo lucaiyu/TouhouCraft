cf8d70f04b885b8b4753a7d4eb234b6fca01c11d
lucaiyu <aiyu2009@126.com>
2022-07-08 18:13:51 +0800

    Texture update and ore test

--- src/main/java/com/lucaiyu/touhoucraft/world/gen/OreGeneration.java
+++ src/main/java/com/lucaiyu/touhoucraft/world/gen/OreGeneration.java
@@ -13,10 +13,10 @@ import net.minecraftforge.event.world.BiomeLoadingEvent;
 
 public class OreGeneration {
     public static void generateOres(final BiomeLoadingEvent event){
-        oreGenerate(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockRegistry.touhou_crystal_ore.get().defaultBlockState(), 1, 6, 0, 7, 1);
+        oreGenerate(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockRegistry.touhou_crystal_ore.get().defaultBlockState(), 16, 6, 0, 7, 16);
         oreGenerate(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockRegistry.magic_ore.get().defaultBlockState(), 1, 1, 0, 5, 1);
         oreGenerate(event.getGeneration(), OreFeatureConfig.FillerBlockType.NETHER_ORE_REPLACEABLES, BlockRegistry.touhou_crystal_ore_in_nether.get().defaultBlockState(), 3, 30, 0, 31, 2);
-        oreGenerate(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockRegistry.magic_ore_in_nether.get().defaultBlockState(), 2, 30, 0, 31, 1);
+        oreGenerate(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE, BlockRegistry.magic_ore_in_nether.get().defaultBlockState(), 16, 30, 0, 31, 16);
     }
     private static void oreGenerate(BiomeGenerationSettingsBuilder settings,
                                     RuleTest fillerType,
