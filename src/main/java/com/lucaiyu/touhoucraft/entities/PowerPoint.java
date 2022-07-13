41049de148edd00b6abc9902fa4879c36c4ac105
lucaiyu <aiyu2009@126.com>
2022-07-13 21:45:40 +0800

    fix entity

--- src/main/java/com/lucaiyu/touhoucraft/entities/PowerPoint.java
+++ src/main/java/com/lucaiyu/touhoucraft/entities/PowerPoint.java
@@ -1,36 +1,15 @@
 package com.lucaiyu.touhoucraft.entities;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.network.IPacket;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.world.World;
 
 
-public class PowerPoint extends Entity {
+public class PowerPoint extends MobEntity {
     public PowerPoint(World worldIn){
         this(null, worldIn);
     }
-    public PowerPoint(EntityType<? extends Entity> type, World worldIn) {
+    public PowerPoint(EntityType<? extends MobEntity> type, World worldIn) {
         super(type, worldIn);
-    }
-    @Override
-    protected void registerData() {
-
-    }
-
-    @Override
-    protected void readAdditional(CompoundNBT compound) {
-
-    }
-
-    @Override
-    protected void writeAdditional(CompoundNBT compound) {
-
-    }
-
-    @Override
-    public IPacket<?> createSpawnPacket() {
-        return null;
     }
 }
--- src/main/java/com/lucaiyu/touhoucraft/entities/render/PowerPointRender.java
+++ src/main/java/com/lucaiyu/touhoucraft/entities/render/PowerPointRender.java
@@ -2,15 +2,16 @@ package com.lucaiyu.touhoucraft.entities.render;
 
 import com.lucaiyu.touhoucraft.TouHouCraft;
 import com.lucaiyu.touhoucraft.entities.PowerPoint;
-import net.minecraft.client.renderer.entity.EntityRenderer;
+import com.lucaiyu.touhoucraft.entities.model.PowerPointModel;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-public class PowerPointRender extends EntityRenderer<PowerPoint> {
+public class PowerPointRender extends MobRenderer<PowerPoint, PowerPointModel> {
     private static final ResourceLocation POWER_POINT_TEXTURE = new ResourceLocation(TouHouCraft.MOD_ID, "texture/entity/power_point.png");
     public PowerPointRender(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn);
-        this.shadowSize = 0.15F;
+        super(renderManagerIn, new PowerPointModel(), 0.8F);
     }
 
     @Override
