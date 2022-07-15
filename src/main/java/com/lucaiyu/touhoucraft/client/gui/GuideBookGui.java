b93caf5d1729cb9ed8bbc36162df6735d79511f4
lucaiyu <aiyu2009@126.com>
2022-07-15 21:45:44 +0800

    fix

--- src/main/java/com/lucaiyu/touhoucraft/client/gui/GuideBookGui.java
+++ src/main/java/com/lucaiyu/touhoucraft/client/gui/GuideBookGui.java
@@ -12,9 +12,7 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class GuideBookGui extends Screen {
-    TextFieldWidget textFieldWidget;
-    Button button;
-    ResourceLocation GUIDE_BOOK_GUI_TEXTURE = new ResourceLocation(TouHouCraft.MOD_ID, "textures/gui/");
+    ResourceLocation GUIDE_BOOK_GUI_TEXTURE = new ResourceLocation(TouHouCraft.MOD_ID, "textures/gui/guide_book.png");
     TranslationTextComponent content = new TranslationTextComponent("gui.touhoucraft.guide_book");
     protected GuideBookGui(ITextComponent titleIn) {
         super(titleIn);
@@ -26,17 +24,18 @@ public class GuideBookGui extends Screen {
         this.renderBackground(matrixStack);
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.minecraft.getTextureManager().bindTexture(GUIDE_BOOK_GUI_TEXTURE);
-        /**
-        *[WARNING} Set UV
-         *SUCCESSFUL
-         **/
         int textureWidth = 145;
         int textureHeight = 179;
-
-        this.blit(matrixStack, 0, 0, 0, 0, 145, 179, textureWidth, textureHeight);
+        /**
+         * method blit
+         * x, y, u, v, width, height, textureWidth, textureHeight
+         **/
+        /**
+         * method drawCenteredString
+         * x, y, colour(HEX)
+         */
+        this.blit(matrixStack, width/2-145, height / 2+179, 0, 0, this.width, this.height, textureWidth, textureHeight);
         drawCenteredString(matrixStack, this.font, content, 0, 0, 0xffffff);
-        this.textFieldWidget.render(matrixStack, mouseX, mouseY, partialTicks);
-        this.button.render(matrixStack, mouseX, mouseY, partialTicks);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
 
@@ -44,7 +43,7 @@ public class GuideBookGui extends Screen {
     public void init(Minecraft minecraft, int width, int height) {
         super.init(minecraft, width, height);
         this.minecraft.keyboardListener.enableRepeatEvents(true);
-        //this.textFieldWidget = new TextFieldWidget(this.font, 0, 0, 0, 0, new TranslationTextComponent("gui.touhoucraft.guide_book"));
+        super.init();
 
     }
 }
