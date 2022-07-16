2b5d027d136635f2056e4c4a24be8603e5246e05
lucaiyu <aiyu2009@126.com>
2022-07-16 21:54:41 +0800

    Change button to changepagebutton

--- src/main/java/com/lucaiyu/touhoucraft/client/gui/GuideBookGui.java
+++ src/main/java/com/lucaiyu/touhoucraft/client/gui/GuideBookGui.java
@@ -4,18 +4,19 @@ import com.lucaiyu.touhoucraft.TouHouCraft;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.widget.button.ChangePageButton;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
+
+import java.util.ArrayList;
 
 public class GuideBookGui extends Screen {
     ResourceLocation GUIDE_BOOK_GUI_TEXTURE = new ResourceLocation(TouHouCraft.MOD_ID, "textures/gui/guide_book_gui.png");
-    Button buttonNextPage;
-    Button buttonPreviousPage;
-    private boolean leftButtonIsActive = false;
-    private boolean rightButtonIsActive = false;
-    //TranslationTextComponent content = new TranslationTextComponent("gui.touhoucraft.guide_book_title");
+    ChangePageButton buttonNextPage;
+    ChangePageButton buttonPreviousPage;
+    ArrayList<ArrayList<?>> pages = new ArrayList<ArrayList<?>>();
+
+    private final boolean pageTurnSounds = false;
     protected GuideBookGui(ITextComponent titleIn) {
         super(titleIn);
     }
@@ -34,38 +35,10 @@ public class GuideBookGui extends Screen {
         int j = 2;
         this.blit(matrixStack, i, j, 0, 0, 192, 192);
         /**
-         * render button texture
-         */
-        if (this.leftButtonIsActive == false){
-            this.blit(matrixStack, i+116, 159, 3, 194, 23, 13);
-        }
-        if (this.leftButtonIsActive == true){
-            this.blit(matrixStack, i+116, 159, 3+23, 194, 46, 13);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            this.leftButtonIsActive = false;
-        }
-        if (this.rightButtonIsActive == false){
-            this.blit(matrixStack, i+116, 159, 3, 194+13, 23, 13);
-        }
-        if (this.rightButtonIsActive == true){
-            this.blit(matrixStack, i+43, 159, 3+23, 194+13, 46, 26);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            this.rightButtonIsActive = false;
-        }
-        /**
          * render other things
          */
         this.buttonNextPage.render(matrixStack, mouseX, mouseY, partialTicks);
         this.buttonPreviousPage.render(matrixStack, mouseX, mouseY, partialTicks);
-        //drawCenteredString(matrixStack, this.font, content, this.width/2-10, 30, 0xeb0505);
 
         super.render(matrixStack, mouseX, mouseY, partialTicks);
         /**
@@ -79,15 +52,9 @@ public class GuideBookGui extends Screen {
     }
     private void initButton(){
         int i = (this.width - 192) / 2;
-        this.buttonNextPage = new Button(i+116, 159, 18, 10, new TranslationTextComponent(" "), (buttonNextPage)->{
-            rightButtonIsActive = true;
-        });
-        this.buttonNextPage.setAlpha(0.0F);
+        this.buttonNextPage = new ChangePageButton(i+116, 159, true, (buttonNextPage)->{}, pageTurnSounds);
         this.addButton(buttonNextPage);
-        this.buttonPreviousPage = new Button(i+43, 159, 18, 10, new TranslationTextComponent(" "), (buttonPerviousPage)->{
-            leftButtonIsActive = true;
-        });
-        this.buttonPreviousPage.setAlpha(0.0F);
+        this.buttonPreviousPage = new ChangePageButton(i+43, 159,  false, (buttonPreviousPage)->{}, pageTurnSounds);
         this.addButton(buttonPreviousPage);
     }
     @Override
