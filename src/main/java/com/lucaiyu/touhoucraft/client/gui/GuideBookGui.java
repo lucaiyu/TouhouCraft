6e5928191de390e5b776c7c17baef0246cc1ea05
lucaiyu <aiyu2009@126.com>
2022-07-16 21:15:13 +0800

    Book system:

--- src/main/java/com/lucaiyu/touhoucraft/client/gui/GuideBookGui.java
+++ src/main/java/com/lucaiyu/touhoucraft/client/gui/GuideBookGui.java
@@ -3,29 +3,71 @@ package com.lucaiyu.touhoucraft.client.gui;
 import com.lucaiyu.touhoucraft.TouHouCraft;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
 public class GuideBookGui extends Screen {
-    ResourceLocation GUIDE_BOOK_GUI_TEXTURE = new ResourceLocation(TouHouCraft.MOD_ID, "textures/gui/guide_book.png");
-    TranslationTextComponent content = new TranslationTextComponent("gui.touhoucraft.guide_book");
+    ResourceLocation GUIDE_BOOK_GUI_TEXTURE = new ResourceLocation(TouHouCraft.MOD_ID, "textures/gui/guide_book_gui.png");
+    Button buttonNextPage;
+    Button buttonPreviousPage;
+    private boolean leftButtonIsActive = false;
+    private boolean rightButtonIsActive = false;
+    //TranslationTextComponent content = new TranslationTextComponent("gui.touhoucraft.guide_book_title");
     protected GuideBookGui(ITextComponent titleIn) {
         super(titleIn);
     }
-
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
+        /**
+         * init renderer
+         */
         this.renderBackground(matrixStack);
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.minecraft.getTextureManager().bindTexture(GUIDE_BOOK_GUI_TEXTURE);
-        int textureWidth = 145;
-        int textureHeight = 179;
+        /**
+         * render book page
+         */
+        int i = (this.width - 192) / 2;
+        int j = 2;
+        this.blit(matrixStack, i, j, 0, 0, 192, 192);
+        /**
+         * render button texture
+         */
+        if (this.leftButtonIsActive == false){
+            this.blit(matrixStack, i+116, 159, 3, 194, 23, 13);
+        }
+        if (this.leftButtonIsActive == true){
+            this.blit(matrixStack, i+116, 159, 3+23, 194, 46, 13);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            this.leftButtonIsActive = false;
+        }
+        if (this.rightButtonIsActive == false){
+            this.blit(matrixStack, i+116, 159, 3, 194+13, 23, 13);
+        }
+        if (this.rightButtonIsActive == true){
+            this.blit(matrixStack, i+43, 159, 3+23, 194+13, 46, 26);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            this.rightButtonIsActive = false;
+        }
+        /**
+         * render other things
+         */
+        this.buttonNextPage.render(matrixStack, mouseX, mouseY, partialTicks);
+        this.buttonPreviousPage.render(matrixStack, mouseX, mouseY, partialTicks);
+        //drawCenteredString(matrixStack, this.font, content, this.width/2-10, 30, 0xeb0505);
+
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
         /**
          * method blit
          * x, y, u, v, width, height, textureWidth, textureHeight
@@ -34,15 +76,24 @@ public class GuideBookGui extends Screen {
          * method drawCenteredString
          * x, y, colour(HEX)
          */
-        this.blit(matrixStack, width/2-145, height / 2+179, 0, 0, this.width, this.height, textureWidth, textureHeight);
-        drawCenteredString(matrixStack, this.font, content, 0, 0, 0xffffff);
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
     }
-
+    private void initButton(){
+        int i = (this.width - 192) / 2;
+        this.buttonNextPage = new Button(i+116, 159, 18, 10, new TranslationTextComponent(" "), (buttonNextPage)->{
+            rightButtonIsActive = true;
+        });
+        this.buttonNextPage.setAlpha(0.0F);
+        this.addButton(buttonNextPage);
+        this.buttonPreviousPage = new Button(i+43, 159, 18, 10, new TranslationTextComponent(" "), (buttonPerviousPage)->{
+            leftButtonIsActive = true;
+        });
+        this.buttonPreviousPage.setAlpha(0.0F);
+        this.addButton(buttonPreviousPage);
+    }
     @Override
-    public void init(Minecraft minecraft, int width, int height) {
-        super.init(minecraft, width, height);
+    public void init() {
         this.minecraft.keyboardListener.enableRepeatEvents(true);
+        initButton();
         super.init();
 
     }
